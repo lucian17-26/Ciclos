14c317fab30ebb3fb2a61037ab7012c679056071
Lucian Gallardo <69860953+lucian17-26@users.noreply.github.com>
2022-04-08 11:03:55 -0300

    Desafio complementario N°1

---
+++ Js/script.js
@@ -0,0 +1,6 @@
+//Ejercicio donde se pasa un numero y se repite el string "Hola" la misma cantidad de veces
+
+let entrada = parseFloat(prompt("Ingrese su numero"));
+for(i=1; i<= entrada; i++){
+    console.log(alert("Hola"));
+}
